4e6048c7895ff04cd5b3ef19655fde9d681d240b
Mehrdad Golkia <mgolkia@munch3.local>
2015-10-28 18:46:16 +0100

    I wrote my first program,yeaaaaaaaaaaaaah

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,15 @@
+//Mehrdad Golkia
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a [30]= {0,1};
+  cout << "0, 1, ";
+  for (int i=2; i<11; i++){
+    a[i]=a[i-1]+a[i-2];
+    cout << a[i] << ", ";
+  }
+  cout << endl;
+  return 0;
+}
 
